faa57b68dd19cc679d747919c1885804682c13d6
Kozhedub-Proger <Kozhedub_pavel@mail.ru>
2026-08-21 01:11:49 +0500

    Harden Android 15 startup and show runtime errors

--- app/src/main/java/com/kozhedub/atlantic4145/MainActivity.java
+++ app/src/main/java/com/kozhedub/atlantic4145/MainActivity.java
@@ -2,17 +2,31 @@ package com.kozhedub.atlantic4145;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
+import android.view.View;
+import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
     @Override public void onCreate(Bundle b) {
         super.onCreate(b);
-        if (android.os.Build.VERSION.SDK_INT >= 30) {
-            getWindow().setDecorFitsSystemWindows(false);
-            WindowInsetsController c = getWindow().getInsetsController();
-            if (c != null) c.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        try {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            setContentView(new NavalGameView(this));
+        } catch (Throwable t) {
+            TextView tv = new TextView(this);
+            tv.setBackgroundColor(Color.rgb(8,18,28));
+            tv.setTextColor(Color.WHITE);
+            tv.setTextSize(16f);
+            tv.setPadding(32,32,32,32);
+            tv.setText("Ошибка запуска:\n\n" + android.util.Log.getStackTraceString(t));
+            setContentView(tv);
         }
-        setContentView(new NavalGameView(this));
     }
 }
